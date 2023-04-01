5139e53c3638f5b1f9839b981644058a78e544df
Ronasian <119774818+Ronasian@users.noreply.github.com>
2023-04-01 10:36:13 -0700

    created post request in server.js for new notes

--- server.js
+++ server.js
@@ -1,15 +1,39 @@
 const express = require('express');
 
-const app = express();
 const PORT = 3001;
-const path = require('path');
 const notesData = require('./db/notes.json');
+
+const path = require('path');
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
 
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html')))
+
 app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '/public/notes.html')));
 
-app.get('/api/notes', (req, res) => res.json(notesData));
+app.get('/api/notes', (req,res) => res.json(notesData));
+
+app.post('/api/notes', (req, res) => {
+  console.info(`${req.method} request recieved to add a note`);
+  
+  let response;
+
+  if (req.body && req.body.title && req.body.text) {
+    response = {
+      status: 'success',
+      data: req.body
+    };
+    res.json(`Note has been added!`);
+  } else {
+    res.json('Note must contain title and text');
+  }
+  console.log(req.body);
+});
 
 app.listen(PORT, () =>
   console.log(`Note Taker App listening at http://localhost:${PORT}`)
