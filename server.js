574e19eb0035297427591664000fdeb4f90bd44d
Ronasian <119774818+Ronasian@users.noreply.github.com>
2023-04-03 15:11:23 -0700

    added delete request route in server.js to delete notes

--- server.js
+++ server.js
@@ -1,27 +1,33 @@
 const express = require('express');
 const fs = require('fs');
-
-const PORT = 3001;
-const notesData = require('./db/notes.json');
-
-const path = require('path');
+const uuid = require('uuid');
 
 const app = express();
+const PORT = 3001;
+
+const notesData = require('./db/notes.json');
+const path = require('path');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
 
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html')))
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'))
+});
 
-app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '/public/notes.html')));
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/notes.html'))
+});
 
-app.get('/api/notes', (req,res) => res.json(notesData));
+app.get('/api/notes', (req,res) => {
+  res.json(notesData)
+});
 
 app.post('/api/notes', (req, res) => {
 // Log that a POST request was received
-  console.info(`${req.method} request received to add a review`);
+  console.info(`${req.method} request received to add a note`);
   // Destructuring assignment for the items in req.body
   const { title, text } = req.body;
 
@@ -31,16 +37,16 @@ app.post('/api/notes', (req, res) => {
     const newNote = {
       title,
       text,
-      // review_id: uuid(),
+      id: uuid.v1(),
     };
 
     // Convert the data to a string so we can save it
     fs.readFile("./db/notes.json", "utf8", (err, data) => {
       if (err) {
         console.error(err);
-        return res.status(500).json('Eror in posting note');
+        return res.status(500).json('Error in posting note');
       } else {
-        const notes = JSON.parse(data);
+        let notes = JSON.parse(data);
         notes.push(newNote);
         // Write the data to file
         fs.writeFile("./db/notes.json", JSON.stringify(notes, null, '\t'), (err) =>
@@ -51,7 +57,7 @@ app.post('/api/notes', (req, res) => {
           )
         );
       }
-    })
+    });
     
     const response = {
       status: 'success',
@@ -63,6 +69,32 @@ app.post('/api/notes', (req, res) => {
   } else {
     res.status(500).json('Error in posting review');
   }
+});
+
+app.delete('/api/notes:id', (req, res) => {
+  console.info(`${req.method} request received to delete a note`);
+  const id = req.params.id;
+  console.info(id);
+  fs.readFile("./db/notes.json", "utf8", (err, data) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).json('Error in deleting note');
+    } else {
+      let notes = JSON.parse(data);
+      for (let i = 0; i < notes.length; i++) {
+        if (id === notes[i].id) {
+          notes.splice(i, 1);
+          fs.writeFile("./db/notes.json", JSON.stringify(notes, null, '\t'), (err) =>
+          err
+            ? console.error(err)
+            : console.log(
+                "Note has been removed from notes.json file"
+              )
+          )
+        }
+      }
+    }
+  })
 });
 
 app.listen(PORT, () =>
