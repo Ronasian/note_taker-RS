4c7e8d7f47b3c4994550cab0e6454ef83d71993a
Ronasian <119774818+Ronasian@users.noreply.github.com>
2023-04-04 00:09:06 -0700

    added comments for server.js code

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ const uuid = require('uuid');
 const app = express();
 const PORT = 3001;
 
-const notesData = require('./db/notes.json');
 const path = require('path');
 
 app.use(express.json());
@@ -13,50 +12,54 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
 
+// home page get request
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, './public/index.html'))
 });
-
+// notes page get request
 app.get('/notes', (req, res) => {
   res.sendFile(path.join(__dirname, './public/notes.html'))
 });
-
+// notes api get request
 app.get('/api/notes', (req,res) => {
-  // res.json(notesData)
+  // reads file and returns data
   fs.readFile("./db/notes.json", "utf8", (err, data) => {
     if (err) {
       console.error(err);
     } else {
+      // parsed data
       let dataObj = JSON.parse(data);
+      // response sends back parsed data
       res.json(dataObj);
     }
   })
 });
 
 app.post('/api/notes', (req, res) => {
-// Log that a POST request was received
   console.info(`${req.method} request received to add a note`);
   // Destructuring assignment for the items in req.body
   const { title, text } = req.body;
 
   // If all the required properties are present
   if (title && text) {
-    // Variable for the object we will save
+    // create variable for the note object we will save
     const newNote = {
       title,
       text,
       id: uuid.v1(),
     };
 
-    // Convert the data to a string so we can save it
+    // Read file
     fs.readFile("./db/notes.json", "utf8", (err, data) => {
       if (err) {
         console.error(err);
         return res.status(500).json('Error in posting note');
       } else {
+        // parse all data from file and save it to a variable
         let notes = JSON.parse(data);
+        // push new note data to notes data
         notes.push(newNote);
-        // Write the data to file
+        // Write the new data to file
         fs.writeFile("./db/notes.json", JSON.stringify(notes, null, '\t'), (err) =>
       err
         ? console.error(err)
@@ -81,17 +84,24 @@ app.post('/api/notes', (req, res) => {
 
 app.delete('/api/notes/:id', (req, res) => {
   console.info(`${req.method} request received to delete a note`);
+  // take id parameter from object
   const id = req.params.id;
   console.info(id);
+  // read file and return data
   fs.readFile("./db/notes.json", "utf8", (err, data) => {
     if (err) {
       console.error(err);
       return res.status(500).json('Error in deleting note');
     } else {
+      // parse data
       let notes = JSON.parse(data);
+      // loop over parsed data
       for (let i = 0; i < notes.length; i++) {
+        // if the id variable matches the id at the current note
         if (id === notes[i].id) {
+          // remove that note object from array
           notes.splice(i, 1);
+          // write new data to file
           fs.writeFile("./db/notes.json", JSON.stringify(notes, null, '\t'), (err) =>
           err
             ? console.error(err)
@@ -101,6 +111,7 @@ app.delete('/api/notes/:id', (req, res) => {
           )
         }
       }
+      // after note is deleted from database, refresh page
       res.redirect("/notes");
     }
   })
